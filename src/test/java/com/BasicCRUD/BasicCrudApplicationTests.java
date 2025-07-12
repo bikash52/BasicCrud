50f5fa50b49e667bd7071b97d71e668c38a8cc04
Your Name <jbikashkumar58@gmail.com>
2025-07-12 10:29:28 +0530

    updated code

--- src/test/java/com/BasicCRUD/BasicCrudApplicationTests.java
+++ src/test/java/com/BasicCRUD/BasicCrudApplicationTests.java
@@ -1,5 +1,7 @@
 package com.BasicCRUD;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,26 +16,39 @@ class BasicCrudApplicationTests {
 
 	@Test
 	void saveUser() {
-		User us=new User();
-		us.setName("Akash");
-		us.setEmail("as=kasj995@gmail.com");
-		us.setMobile("9114158927");
+		User us = new User();
+		us.setName("Bikash");
+		us.setEmail("bikashj995@gmail.com");
+		us.setMobile("9114165927");
 		user.save(us);
 	}
-	
+
 	@Test
 	void deleteUser() {
-		User us=new User();
 		user.deleteById(3L);
 	}
 
 	@Test
 	void updateUser() {
-		User us=new User();
+		User us = new User();
 		us.setId(2L);
 		us.setName("Ankita");
 		us.setEmail("ankitamohaptra532@gmail.com");
 		us.setMobile("8328857252");
 		user.save(us);
+	}
+
+	@Test
+	void getUser() {
+		Optional<User> val = user.findById(1L);
+		if (val.isPresent()) {
+			User us = new User();
+			System.out.println(us.getId());
+			System.out.println(us.getName());
+			System.out.println(us.getEmail());
+			System.out.println(us.getMobile());
+		} else {
+			System.out.println("No record found");
+		}
 	}
 }
